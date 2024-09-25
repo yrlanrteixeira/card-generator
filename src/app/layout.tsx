8da1bc49d0322bada53671be174a21f707576ebc
Yrlan R Teixeira <yrlan.01@hotmail.com>
2024-09-25 16:07:46 -0300

    refactor: Update metadata in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,19 @@ const darkerGrotesque = Darker_Grotesque({ subsets: ['latin'], variable: '--font
 export const metadata = {
   title: 'Gerador de Cartão de Visita',
   description: 'Crie seu cartão de visita digital de maneira rápida e fácil.',
+  keywords: 'cartão de visita, cartão de visita digital, gerador de cartão de visita',
+
+  ogTitle: 'Gerador de Cartão de Visita',
+  ogDescription: 'Crie seu cartão de visita digital de maneira rápida e fácil.',
+  ogImage: '/og-image.png', // Valores que serão substituídos pelos valores reais
+  ogType: 'website',
+  ogSiteName: 'Gerador de Cartão de Visita',
+
+  twitterCard: 'summary_large_image',
+  twitterTitle: 'Gerador de Cartão de Visita',
+  twitterDescription: 'Crie seu cartão de visita digital de maneira rápida e fácil.',
+  twitterImage: '/twitter-image.png', // Valores que serão substituídos pelos valores reais
+  twitterSite: '@rdstation',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
